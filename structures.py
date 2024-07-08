70e5304b9b3895887676d33c289799150a43ab1c
CrownCraftedContent <crowncraftedcontent@gmail.com>
2024-07-07 23:47:29 -0500

    Notes on the auto-fill popup helper

--- structures.py
+++ structures.py
@@ -2,11 +2,15 @@
 # Lists, Tuples, Etc.
 
 # LISTS
-print("LISTS")
+print("\nLISTS")
+#   arrays (adjustable storage containers)
 food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]  # set list
 food[0] = "sushi"  # adjust an element
 
 food.append("ice cream")
+print(food)
+print(food.__class__)
+
 food.remove("hotdog")
 food.pop()  # removes the last element
 food.insert(0, "cake")  # adds to a certain index
@@ -14,8 +18,10 @@ food.sort()
 food.clear()
 
 # TUPLES
-print("TUPLES")
+print("\nTUPLES")
 #   a Tuple is a collection which is ordered and unchangeable
 #   is used to group together related data
 student = ("Crown", 21, "male")
-print("\tOccurrences" + str(student.count("Crown")))
+print(student.count("Crown"))
+# print("\tOccurrences" + str(student.count("Crown")))
+print(student.index("male"))
