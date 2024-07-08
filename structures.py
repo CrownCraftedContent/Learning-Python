0034cd6211ce8997dde206c6231290dcffb29a79
CrownCraftedContent <crowncraftedcontent@gmail.com>
2024-07-08 00:14:53 -0500

    Sets

--- structures.py
+++ structures.py
@@ -22,6 +22,25 @@ print("\nTUPLES")
 #   a Tuple is a collection which is ordered and unchangeable
 #   is used to group together related data
 student = ("Crown", 21, "male")
-print(student.count("Crown"))
-# print("\tOccurrences" + str(student.count("Crown")))
+print(student.count("Crown"))  # print("\tOccurrences" + str(student.count("Crown")))
 print(student.index("male"))
+for x in student:
+    print(x, end=" ")
+if "Crown" in student:
+    print("Crown is here!")
+
+# SETS
+print("\nSETS")
+#   a collection which is unordered, unindexed, and does not retain duplicate values
+#   faster than a list in search time
+utensils = {"fork", "spoon", "knife", "knife"}  # doesn't keep extra knife
+print("Size of utensils set = " + str(len(utensils)))
+utensils.add("napkin")
+utensils.remove("fork")
+print("Utensils = " + str(utensils))
+utensils.clear()
+print("Utensils = " + str(utensils))  # prints set() which is the constructor of its class
+dishes = {"bowl", "plate", "cup"}
+# add one set to another using the update method
+utensils.update(dishes)
+print("Utensils = " + str(utensils))
