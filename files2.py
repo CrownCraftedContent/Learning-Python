87ac874a11b07b2ada7132943ce57e41183403bd
CrownCraftedContent <crowncraftedcontent@gmail.com>
2024-07-09 10:57:30 -0500

    File movement/deletion

--- files2.py
+++ files2.py
@@ -1,4 +1,4 @@
-import shutil
+# import shutil
 
 # FILE COPYING
 # copyfile() = copies contents of a file
@@ -7,4 +7,44 @@ import shutil
 # ^ all take the same arguments
 
 # shutil.copyfile()  # src, dst
-shutil.copyfile("C:\\Users\\thepm\\Desktop\\TextForPython.txt", 'copy.txt')  # just a name copies to project path
+# shutil.copyfile("C:\\Users\\thepm\\Desktop\\TextForPython.txt", 'copy.txt')  # just a name copies to project path
+
+
+import os
+import shutil
+
+# FILE MOVING
+print("\nFILE MOVING")
+source = "test.txt"
+destination = "C:\\Users\\thepm\\OneDrive\\Desktop\\test.txt"
+
+try:
+    if os.path.exists(destination):  # <-- prevents overwriting on accident
+        print("There is a file here!")
+    else:
+        os.replace(source, destination)  # moves files and directories
+        print(source + " was moved to " + destination)
+except FileNotFoundError:
+    print(source + " was not found.")
+
+
+# FILE DELETION
+# os used for files and empty directories
+# shutil to remove a directory containing files
+path = 'test.txt'  # if within project
+# os.remove(path)
+
+path2 = 'folder'
+# to be careful v
+try:
+    #os.remove(path)  # delete files
+    os.rmdir(path2)  # delete EMPTY directories only
+    #shutil.rmtree(path2)  # delete dir containing files. considered dangerous because removes all contents
+except FileNotFoundError:
+    print("That file was not found.")
+except PermissionError:  # thrown when trying to remove an empty folder (directory)
+    print("You do not have permission to delete that")
+except OSError:
+    print("You cannot delete that using that function")
+else:
+    print(path2 + " was deleted")
