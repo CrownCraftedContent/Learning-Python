cbdef7514748a466bef1174b9cbce27effc74749
CrownCraftedContent <crowncraftedcontent@gmail.com>
2024-07-09 06:07:11 -0500

    *args, **kwargs and localization/scope

--- functions.py
+++ functions.py
@@ -24,3 +24,16 @@ def add(*stuff):  # or *args
     return total
 
 print(add(1,2,3))
+
+# **kwargs
+#       parameter that will unpack all arguments into a dictionary
+#       useful so that a function can accept a varying amount of keyword arguments
+#       kwargs is common naming convention but ** is important piece
+
+def hello(**kwargs):
+    #  print("Hello", kwargs.get('first'), kwargs.get('last'))
+    print("Hello", end=" ")
+    for key, value in kwargs.items():
+        print(value, end=" ")
+
+hello(first="Jack", last="Johnson", extra="The 3rd")
