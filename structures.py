835fa532bfe745c70059a343276773b82e37854a
CrownCraftedContent <crowncraftedcontent@gmail.com>
2024-07-10 05:46:13 -0500

    List comprehension

--- structures.py
+++ structures.py
@@ -65,15 +65,22 @@ capitals.pop('India')
 capitals.clear()
 
 
-print("\nLISTS CONTINUED")
+print("\nLISTS CONTINUED (with comprehension)")
 # list comprehension = a way to create a new list with less syntax
 #       can mimic certain lambda functions, easier to read
 #       list = [expression for item in iterable]
+#       list = [expression for item in iterable if conditional]
+#       list = [expression if/else for item in iterable]
 
 squares = []
 for i in range(1,11):
     squares.append(i * i)
 print(squares)
 
-squares = [i * i for i in range(1,11)]  # equivalent to code above
+squares = [i * i for i in range(1,11)]  # equivalent for loop above
 print(squares)
+
+students = [100,90,80,70,60,50,40,30,0]
+# passed_students = [i for i in students if i >= 60]
+passed_students = [i if i >= 60 else "Failed" for i in students]
+print(passed_students)
